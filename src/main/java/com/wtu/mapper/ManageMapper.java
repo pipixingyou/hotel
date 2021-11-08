65fba8127457c5ea32f82e0e46e90038341743d1
pipixingyou <72735220+pipixingyou@users.noreply.github.com>
2021-11-08 11:49:10 +0800

    Update ManageMapper.java

--- src/main/java/com/wtu/mapper/ManageMapper.java
+++ src/main/java/com/wtu/mapper/ManageMapper.java
@@ -6,6 +6,9 @@ import org.apache.ibatis.annotations.Param;
 public interface ManageMapper {
     //登录
     Manage MangeLogin(@Param("mcode") String mcode,@Param("password") String password);
+    
+    //测试接口
+    void myTest();
 
 
 }
